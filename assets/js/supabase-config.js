77f468627f4d261b33a142f36eb51a87e7af850a
eyfafajay <ffarhanah03@gmail.com>
2026-06-10 18:12:17 +0800

    correct Supabase URL constant typo in client initialization

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,10 +1,10 @@
 // assets/js/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const  SUPABASE_URL = 'https://drvmfnlaxkcqbwoqjefu.supabase.co';
+const SUPABASE_URL = 'https://drvmfnlaxkcqbwoqjefu.supabase.co';
 const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_4ugmgc1ktCaLEmwB1ttnbA_XjOCtqDm';
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
     persistSession: true,
     autoRefreshToken: true
